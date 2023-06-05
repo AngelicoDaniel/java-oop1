439209eb2a7b43f8dfeacc9c893b825927935886
Daniel <angelicodaniel93@gmail.com>
2023-06-05 17:43:34 +0200

    last commit

--- src/org/lessons/java/bank/Bank.java
+++ src/org/lessons/java/bank/Bank.java
@@ -14,28 +14,34 @@ public class Bank {
         System.out.println("Benvenuto, " + conto.getNomeProprietario());
         System.out.println("Informazioni conto: " + conto.getInformazioniConto());
 
-        System.out.println("Cosa vuoi fare?");
-        System.out.println("1. Versare una somma di denaro");
-        System.out.println("2. Prelevare una somma di denaro");
-        System.out.println("3. Uscire");
-        System.out.println("Scelta: ");
-        int scelta = scanner.nextInt();
+        boolean continua = true;
+        while (continua) {
+            System.out.println("\nCosa vuoi fare?");
+            System.out.println("1. Versare una somma di denaro");
+            System.out.println("2. Prelevare una somma di denaro");
+            System.out.println("3. Uscire");
+            System.out.print("Scelta: ");
+            int scelta = scanner.nextInt();
 
-        switch (scelta) {
-            case 1:
-                System.out.println("Importo da versare: ");
-                double importoVersato = scanner.nextDouble();
-                conto.versa(importoVersato);
-                System.out.println("Saldo attuale: " + conto.getInformazioniConto());
-                break;
-            case 2:
-                System.out.println("Importo da prelevare: ");
-                double importoPrelevato = scanner.nextDouble();
-                conto.preleva(importoPrelevato);
-                System.out.println("Saldo attuale: " + conto.getInformazioniConto());
-                break;
-            case 3:
-                break;
+            switch (scelta) {
+                case 1:
+                    System.out.print("Importo da versare: ");
+                    double importoVersato = scanner.nextDouble();
+                    conto.versa(importoVersato);
+                    System.out.println("Saldo attuale: " + conto);
+                    break;
+                case 2:
+                    System.out.print("Importo da prelevare: ");
+                    double importoPrelevato = scanner.nextDouble();
+                    conto.preleva(importoPrelevato);
+                    System.out.println("Saldo attuale: " + conto);
+                    break;
+                case 3:
+                    continua = false;
+                    break;
+                default:
+                    System.out.println("Scelta non valida. Riprova.");
+            }
         }
 
         scanner.close();
